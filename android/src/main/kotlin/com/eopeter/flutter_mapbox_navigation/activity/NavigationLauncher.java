98252c52a8bc3ade4d8ddfb49bea6eedcd9f80b7
Ivan Tuang <fuanliang.tuang@compasia.com>
2023-03-01 17:00:58 +0800

    test

--- android/src/main/kotlin/com/eopeter/flutter_mapbox_navigation/activity/NavigationLauncher.java
+++ android/src/main/kotlin/com/eopeter/flutter_mapbox_navigation/activity/NavigationLauncher.java
@@ -2,6 +2,8 @@ package com.eopeter.flutter_mapbox_navigation.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.app.PendingIntent;
+import android.os.Build;
 import com.mapbox.geojson.Point;
 import java.io.Serializable;
 import java.util.List;
@@ -13,7 +15,20 @@ public class NavigationLauncher {
     public static void startNavigation(Activity activity, List<Point> wayPoints) {
         Intent navigationIntent = new Intent(activity, NavigationActivity.class);
         navigationIntent.putExtra("waypoints", (Serializable) wayPoints);
-        activity.startActivity(navigationIntent);
+//        activity.startActivity(navigationIntent);
+
+        try {
+            int flags = PendingIntent.FLAG_UPDATE_CURRENT;
+            // should be >= S, but there is a S device which SDK_INT==R
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                flags |= PendingIntent.FLAG_IMMUTABLE;
+            }
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(activity, 0, navigationIntent, flags);
+            pendingIntent.send();
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void stopNavigation(Activity activity) {
